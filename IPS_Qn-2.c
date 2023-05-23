c5ef4b719fb5a58bd389bf45083273f741e709db
Suman <sangitsuman987@gmail.com>
2023-05-23 17:26:58 +0530

    Please enter the commit message for your changes. Lines starting
    cd

---
+++ IPS_Qn-2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    for (int i=0; i<n; i++){
+        printf("Enter the no. of balls in bag %d: ", i+1);
+        scanf("%d", &arr[i]);
+    }
+    
+    int sum=0;
+    for (int i=0; i<n-1; i++){
+        printf("%d, ", arr[i]);
+        sum+=arr[i];
+    }
+    printf("%d\n", arr[n-1]);
+    printf("%d\n", arr[n-1]+sum);
+    return 0;
+}
